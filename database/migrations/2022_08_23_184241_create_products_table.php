d15365d32eca620100ab8efa91cb01b32dc2fe9d
DanielC19 <danysam1010@gmail.com>
2022-09-05 22:44:04 -0500

    Feature: Basic migrations
    - Add basic migrations. This can change or add migrations.

--- database/migrations/2022_08_23_184241_create_products_table.php
+++ database/migrations/2022_08_23_184241_create_products_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2022_08_23_185005_create_presentations_table.php
+++ database/migrations/2022_08_23_185005_create_presentations_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('presentations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('product_id')->constrained();
+            $table->string('amount');
+            $table->bigInteger('price');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('presentations');
     }
 };
