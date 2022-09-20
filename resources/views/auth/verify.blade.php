9d9ecce7f4321efd62954139d584eae7b2e9b622
DanielC19 <danysam1010@gmail.com>
2022-09-20 17:11:37 -0500

    Feature: Laravel Login
    - Add Laravel UI login
    - Add some customization to login views
    - Overcharge logout route (originally is set to POST by the library, but it needs to be GET).

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.header')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Verifica tu email</div>
+
+                <div class="card-body">
+                    @if (session('resent'))
+                        <div class="alert alert-success" role="alert">
+                            Se ha enviado un nuevo link de recuperación a tu email
+                        </div>
+                    @endif
+
+                    Antes de continuar, revisa tu email por un link de verificación.
+                    Si no recibiste tu email...
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">enviar otro link</button>.
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+// Overcharge logout route, it appears to be POST originally and it cannot be
+Route::get('logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
