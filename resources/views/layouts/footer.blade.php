a8d53694ce3e38d7d33127f0cd34d8af764011dc
DanielC19 <danysam1010@gmail.com>
2022-09-29 18:09:02 -0500

    Feature: Footer
    - Add footer component
    - Add footer styles (banner too, forgot to do in home view commit)
    - Needs actual links on its buttons, but currently there aren't

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,21 @@
+<footer class="footer">
+    <div class="tile is-ancestor">
+        <div class="tile is-vertical has-text-centered p-5">
+            <h5 class="mb-4">Obtén un ícono de <b>peludos</b> en tu página de inicio</h4>
+            <div class="container">
+                <button class="button is-rounded"><i class="fa-solid fa-download fa-2xl"></i></button>
+            </div>
+        </div>
+        <div class="tile is-vertical has-text-centered p-5">
+            <h5 class="mb-4">Encuéntranos en</h5>
+            <div class="container">
+                <button class="button is-rounded m-2"><i class="fa-brands fa-instagram fa-2xl"></i></button>
+                <button class="button is-rounded m-2"><i class="fa-brands fa-whatsapp fa-2xl"></i></button>
+                <button class="button is-rounded m-2"><i class="fa-brands fa-facebook fa-2xl"></i></button>
+            </div>
+        </div>
+        <div class="tile is-vertical center p-5">
+            <img src="{{ Vite::asset('resources/img/logo-blanco.png') }}" class="logo-brand" alt="Logo Peludos">
+        </div>
+    </div>
+</footer>
