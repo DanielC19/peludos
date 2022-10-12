e795cce155c2caa30684613f50c37076fa384664
DanielC19 <danysam1010@gmail.com>
2022-10-11 20:46:47 -0500

    Fix: Category buttons
    - Change a class to fix category buttons to overflow from view in mobile

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,7 +5,7 @@
 <h2 class="title is-2 center my-5">{{ $category->animal->name }}</h2>
 
 {{-- Categories --}}
-<section class="categories center">
+<section class="is-flex-wrap-wrap center">
     @foreach ($category->animal->categories as $category_filters)
         @if ($category_filters->id == $category->id)
             <a href="{{ route('category', $category_filters->id) }}" class="button is-primary is-rounded m-2"><i class="fa-solid fa-bone mr-2"></i>{{ $category_filters->name }}</a>    
