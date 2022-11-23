feeb2bb8d754fe952159c8c844ea1e02058455b7
DanielC19 <danysam1010@gmail.com>
2022-11-23 12:20:53 -0500

    Feature: Add fields to user
    - Add cellphone and address fields to users' database and model for billing and shipping

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->bigInteger('cellphone')->nullable();
+            $table->string('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
