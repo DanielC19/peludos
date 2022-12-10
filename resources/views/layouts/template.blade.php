4d30ec74e420d4bacc077164b50295062a5287ee
DanielC19 <danysam1010@gmail.com>
2022-12-10 14:14:21 -0500

    Feature: Convert into PWA

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -39,5 +40,14 @@
 {{-- Livewire JS scripts --}}
 @livewireScripts
 
+{{-- PWA / Service worker scripts --}}
+<script src="{{ asset('/sw.js') }}"></script>
+<script>
+    if (!navigator.serviceWorker.controller) {
+        navigator.serviceWorker.register("/sw.js").then(function (reg) {
+            console.log("Service worker has been registered for scope: " + reg.scope);
+        });
+    }
+</script>
 </body>
 </html>
