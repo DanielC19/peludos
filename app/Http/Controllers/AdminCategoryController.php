409563944948741ebbc80b8a111f3e239dbc437e
DanielC19 <danysam1010@gmail.com>
2023-02-02 11:01:38 -0500

    Fix: Eager load admin categories

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -19,7 +19,7 @@ class AdminCategoryController extends AdminController
      */
     public function index()
     {
-        $categories = Category::paginate();
+        $categories = Category::with('animal')->paginate();
 
         return view('admin.category.index', compact('categories'))
             ->with('i', (request()->input('page', 1) - 1) * $categories->perPage());
