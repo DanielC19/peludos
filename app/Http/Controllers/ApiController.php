2448e625196333012187e8536dc8ad7460dd0e35
DanielC19 <danysam1010@gmail.com>
2024-07-01 19:24:09 -0500

    Feature: Upload products

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,31 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Presentation;
+use App\Models\Product;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ApiController
 {
     /**
-     * Uploads product image, from scraping
-     * returns image name to save in DB from scraping script
+     * Uploads products with image and presentation
      */
-    public function uploadImage(Request $request) : string
+    public function uploadProduct(Request $request)
     {
         try {
-            $timestamp = Carbon::now()->timestamp;
-            $filename = "product_$timestamp.jpg";
-            $image = $request->image->storeAs('uploads', $filename, 'public');            
-            
-            return json_encode([
-                'res' => 'OK',
-                'image' => $image
+            $request->validate([
+                'name' => 'required',
+                'category_id' => 'required',
+                'image' => 'required',
+                'presentations' => 'required',
             ]);
-        } catch (\Exception $e) {
-            return json_encode([
-                'res' => 'OK',
-                'image' => $image
+
+            $product = Product::create([
+                'id' => Product::generateId(),
+                'category_id' => $request->category_id,
+                'name' => $request->name,
+                'image' => $request->image,
+                'availability' => 1,
             ]);
+
+            foreach ($request->presentations as $presentation) {
+                Presentation::create([
+                    'id' => Presentation::generateId(),
+                    'product_id' => $product->id,
+                    'amount' => $presentation['amount'],
+                    'price' => $presentation['price'],
+                    'availability' => 1,
+                ]);
+            }
+
+            return response()->json(['message' => 'ok']);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => true, 'message' => $th->getMessage()], 400);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('upload/image', [ApiController::class, 'uploadImage']);
+Route::post('upload/product', [ApiController::class, 'uploadProduct']);
